553b33c2dbb948c558059a5a382c0656da997428
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-30 22:49:01 +1100

    Can now use intents

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const GetElonTweetHandler = {
       .speak(speakOutput)
       // Uncomment the next line if you want to keep the session open so you can
       // ask for another fact without first re-opening the skill
-      // .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
       .withSimpleCard(requestAttributes.t('SKILL_NAME'), randomFact)
       .getResponse();
   },
